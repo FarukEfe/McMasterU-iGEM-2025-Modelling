c4b8c3f8ec5f19d1c3686cbf37b34be1d266a31c
FarukEfe <efefrk30@gmail.com>
2025-07-01 17:01:15 -0400

    fix: test & debug setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,6 +2,14 @@
 
 # Make sure we're on linux
 $ {OSTYPE} != "linux-gnu" && echo "This script is intended for Linux systems only." && exit 1
+
+echo "Searching for conda package manager ... (may take 1-2 mins)"
+# Search for the conda package manager in the os, show loading bar in the process, leave error message if not found and exit
+if ! command -v conda &> /dev/null; then
+    echo "Conda package manager not found. Please install Anaconda or Miniconda."
+    exit 1
+fi
+
 # Set up venv
 python3 -m venv venv
 venv/Scripts/activate
@@ -16,5 +24,6 @@ $PIP_CMD install --upgrade pip
 $PIP_CMD install -r requirements.txt
 # Deactivate the venv
 deactivate
-# Se up .conda
+
+# Set up .conda
 conda create -n fbenv python=3.11 ncbi-datasets-cli blast orthofinder gemoma memote -y
