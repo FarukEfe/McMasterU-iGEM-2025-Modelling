99c8718518ad2a781633d37190ba3809a12aa0f2
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2025-07-31 12:49:38 -0400

    feat: get rxns based on enzyme list

--- data/altered/test.py
+++
@@ -1,8 +0,0 @@
-from data.altered.parser import parse_obj
-from data.altered.model import *
-
-if __name__ == "__main__":
-    path ='./data/altered/rxns.json'
-    rxns: list[Rxn] = parse_obj(path)
-    for rxn in rxns:
-        print(rxn)
